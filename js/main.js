fb15810ca80f934342337730b7023a13cd1c3cff
Amir-abbas Abdolali <amirabbas.abdolali@gmail.com>
2014-08-16 16:55:18 +0430

    theme changes and sidebar follows offcanvas model

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+	$('.sidebar-btn, .sidebar-close').click(function() {
+		$('body').toggleClass('sidebar-reveal');
+	});
+});
+
+$(document).ready(function() {
+	$('.about-me-btn, .modal-close').click(function() {
+		$('html').toggleClass('perspective');
+	});
+});
+
+
+
